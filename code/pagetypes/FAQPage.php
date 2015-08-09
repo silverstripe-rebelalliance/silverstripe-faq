900b0d0b16c5b9cad299351da86ad23e7ce4dadb
Denise Rivera <denise@silverstripe.com>
2015-08-10 09:24:19 +1200

    phpdocs in faqpage, remove unused function

--- code/pagetypes/FAQPage.php
+++ code/pagetypes/FAQPage.php
@@ -1,16 +1,18 @@
 <?php
-
+/**
+ * FAQ pagetype, displays Q & A related to the page.
+ * Has a custom search index to add search capabilities to the page.
+ * Can live in any part of the SiteTree
+ */
 class FAQPage extends Page {
 	private static $singular_name = 'FAQ Page';
 
-	private static $description = 'FAQ search page with answers.';
-
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-		return $fields;
-	}
+	private static $description = 'FAQ search page';
 }
 
+/**
+ *
+ */
 class FAQPage_Controller extends Page_Controller {
 
 }
