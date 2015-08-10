d65738ba97647d3bdf80ae1ce9233d914992209a
Wilfred Godfrey <w.j.godfrey@gmail.com>
2015-08-10 12:12:31 +1200

    Tidy up base tests.

--- tests/unit/FAQPageTest.php
+++ tests/unit/FAQPageTest.php
@@ -3,25 +3,25 @@
  * Tests basic functionality of FAQPage
  */
 class FAQPageTest extends FunctionalTest {
-    public function setUp() {
-        parent::setUp();
+	public function setUp() {
+		parent::setUp();
 
-        // create faq page
-        $page = new FAQPage(array('Title' => "FAQ Page 1"));
-        $page->write();
-        $page->publish('Stage', 'Live');
+		// create faq page
+		$page = new FAQPage(array('Title' => "FAQ Page 1"));
+		$page->write();
+		$page->publish('Stage', 'Live');
 
-        $this->controller = Injector::inst()->create('FAQPage_Controller');
-    }
+		$this->controller = Injector::inst()->create('FAQPage_Controller');
+	}
 
 	/**
 	 * Basic load page test
 	 */
-    public function testIndex() {
-        $page = $this->get('faq-page-1/');
+	public function testIndex() {
+		$page = $this->get('faq-page-1/');
 
-        // faq page should load..
-        $this->assertEquals(200, $page->getStatusCode());
-    }
+		// faq page should load..
+		$this->assertEquals(200, $page->getStatusCode());
+	}
 
 }
