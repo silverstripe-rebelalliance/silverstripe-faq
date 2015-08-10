5efe8b719e2405c9f45e93bdf6b8d5f19d62edfd
Denise Rivera <denise@silverstripe.com>
2015-08-10 12:01:45 +1200

    basic test coverage for FAQPage

---
+++ tests/unit/FAQPageTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Tests basic functionality of FAQPage
+ */
+class FAQPageTest extends FunctionalTest {
+    public function setUp() {
+        parent::setUp();
+
+        // create faq page
+        $page = new FAQPage(array('Title' => "FAQ Page 1"));
+        $page->write();
+        $page->publish('Stage', 'Live');
+
+        $this->controller = Injector::inst()->create('FAQPage_Controller');
+    }
+
+	/**
+	 * Basic load page test
+	 */
+    public function testIndex() {
+        $page = $this->get('faq-page-1/');
+
+        // faq page should load..
+        $this->assertEquals(200, $page->getStatusCode());
+    }
+
+}
