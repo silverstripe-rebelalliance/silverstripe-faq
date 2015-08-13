f6890dca98011de376d1e5ae859ac9b15c607b86
Wilfred Godfrey <w.j.godfrey@gmail.com>
2015-08-13 16:12:51 +1200

    Break up the index search function.

--- code/pagetypes/FAQPage.php
+++ code/pagetypes/FAQPage.php
@@ -96,7 +96,7 @@ class FAQPage_Controller extends Page_Controller {
 		if ($faq === null) {
 			$this->httpError(404);
 		}
-		
+
 		return array('FAQ' => $faq);
 	}
 
@@ -106,100 +106,142 @@ class FAQPage_Controller extends Page_Controller {
 	 * Search action taken from BasePage.php and modified.
 	 */
 	public function index() {
+		// render normally if no search term
+		if(!$this->request->getVar(self::$search_term_key)) {
+			return $this->render();
+		// otherwise do search
+		} else {
+			return $this->search();
+		}
+	}
+
+	/**
+	 * Search function. Called from index() if we have a search term.
+	 * @return HTMLText search results template.
+	 */
+	public function search() {
 		$start = $this->request->getVar('start') or 0;
 		$limit = self::$results_per_page;
 		$results = new ArrayList();
 		$suggestion = null;
 		$keywords = $this->request->getVar(self::$search_term_key) or '';
 
-		// render normally if no search term
-		if(!$keywords) {
-			return $this->render();
-		// otherwise do search
-		} else {
-			$searchKeywords = preg_replace('/\?$/', '\?', $keywords); // stop Solr breaking questions
-			$query = new SearchQuery();
-			$query->classes = self::$classes_to_search;
-			$query->search($searchKeywords);
+		// get search query
+		$query = $this->getSearchQuery($keywords);
 
-			// Artificially lower the amount of results to prevent too high resource usage.
-			// on subsequent canView check loop.
-			$query->limit(100);
-
-			try {
-				$result = singleton(self::$search_index_class)->search(
-					$query,
-					$start,
-					$limit,
-					array(
-						'defType' => 'edismax',
-						'hl' => 'true',
-						'spellcheck' => 'true',
-						'spellcheck.collate' => 'true'
-					)
-				);
-
-				$results = $result->Matches;
-				$suggestion = $result->Suggestion;
-			} catch(Exception $e) {
-				SS_Log::log($e, SS_Log::WARN);
-			}
-
-			// Clean up the results.
-			foreach($results as $result) {
-				if(!$result->canView()) $results->remove($result);
-			}
-
-			// Generate links
-			$searchURL = Director::absoluteURL(Controller::join_links(
-				Director::baseURL(),
-				$this->Link(),
-				sprintf('?%s=', self::$search_term_key).rawurlencode($keywords)
-			));
-			$rssUrl = Controller::join_links($searchURL, '?format=rss');
-			RSSFeed::linkToFeed($rssUrl, 'Search results for "' . $keywords . '"');
-			$atomUrl = Controller::join_links($searchURL, '?format=atom');
-			CwpAtomFeed::linkToFeed($atomUrl, 'Search results for "' . $keywords . '"');
-
-			/**
-			 * generate the search summary using string replacement
-			 * to support translation and max configurability
-			 */
-			$searchSummary = _t('FAQPage.SearchResultsSummary', $this->SearchResultsSummary);
-			$keys = array(
-				self::$search_results_summary_current_page_key,
-				self::$search_results_summary_total_pages_key,
-				self::$search_results_summary_query_key
-			);
-			$values = array(
-				$results->CurrentPage(),
-				$results->TotalPages(),
-				$keywords
-			);
-			$searchSummary = str_replace($keys, $values, $searchSummary);
-
-			$renderData = array(
-				'SearchResults' => $results,
-				'SearchSummary' => $searchSummary,
-				'Suggestion' => DBField::create_field('Text', $suggestion),
-				'Query' => DBField::create_field('Text', $keywords),
-				'SearchLink' => DBField::create_field('Text', $searchURL),
-				'RSSLink' => DBField::create_field('Text', $rssUrl),
-				'AtomLink' => DBField::create_field('Text', $atomUrl)
-			);
-
-			$templates = array('FAQPage_results', 'Page');
-			if ($this->request->getVar('format') == 'rss') {
-				array_unshift($templates, 'Page_results_rss');
-			}
-			if ($this->request->getVar('format') == 'atom') {
-				array_unshift($templates, 'Page_results_atom');
-			}
-
-			return $this->owner->customise($renderData)->renderWith($templates);
+		try {
+			$this->doSearch($results, $suggestion, $query, $start, $limit, $keywords);
+		} catch(Exception $e) {
+			SS_Log::log($e, SS_Log::WARN);
 		}
+
+		return $this->renderSearch($results, $suggestion, $keywords);
+
 	}
 
+	/**
+	 * Builds a search query from a give search term.
+	 * @return SearchQuery
+	 */
+	public function getSearchQuery($keywords) {
+		// stop Solr breaking questions
+		$searchKeywords = preg_replace('/\?$/', '\?', $keywords);
+		
+		$query = new SearchQuery();
+		$query->classes = self::$classes_to_search;
+		$query->search($searchKeywords);
+
+		// Artificially lower the amount of results to prevent too high resource usage.
+		// on subsequent canView check loop.
+		$query->limit(100);
+
+		return $query;
+	}
+
+	/**
+	 * Performs a search against the configured Solr index from a given query, start and limit.
+	 * Returns $result and $suggestion - both of with are passed by reference.
+	 */
+	public function doSearch(&$results, &$suggestion, $query, $start, $limit) {
+		$result = singleton(self::$search_index_class)->search(
+			$query,
+			$start,
+			$limit,
+			array(
+				'defType' => 'edismax',
+				'hl' => 'true',
+				'spellcheck' => 'true',
+				'spellcheck.collate' => 'true'
+			)
+		);
+
+		// these are both passed by reference and are returned.
+		$results = $result->Matches;
+		$suggestion = $result->Suggestion;
+	}
+
+	/**
+	 * Renders the search template from a given Solr search result, suggestion and search term.
+	 * @return HTMLText search results template.
+	 */
+	public function renderSearch($results, $suggestion, $keywords) {
+		// Clean up the results.
+		foreach($results as $result) {
+			if(!$result->canView()) $results->remove($result);
+		}
+
+		// Generate links
+		$searchURL = Director::absoluteURL(Controller::join_links(
+			Director::baseURL(),
+			$this->Link(),
+			sprintf('?%s=', self::$search_term_key).rawurlencode($keywords)
+		));
+		$rssUrl = Controller::join_links($searchURL, '?format=rss');
+		RSSFeed::linkToFeed($rssUrl, 'Search results for "' . $keywords . '"');
+		$atomUrl = Controller::join_links($searchURL, '?format=atom');
+		CwpAtomFeed::linkToFeed($atomUrl, 'Search results for "' . $keywords . '"');
+
+		/**
+		 * generate the search summary using string replacement
+		 * to support translation and max configurability
+		 */
+		$searchSummary = _t('FAQPage.SearchResultsSummary', $this->SearchResultsSummary);
+		$keys = array(
+			self::$search_results_summary_current_page_key,
+			self::$search_results_summary_total_pages_key,
+			self::$search_results_summary_query_key
+		);
+		$values = array(
+			$results->CurrentPage(),
+			$results->TotalPages(),
+			$keywords
+		);
+		$searchSummary = str_replace($keys, $values, $searchSummary);
+
+		$renderData = array(
+			'SearchResults' => $results,
+			'SearchSummary' => $searchSummary,
+			'Suggestion' => DBField::create_field('Text', $suggestion),
+			'Query' => DBField::create_field('Text', $keywords),
+			'SearchLink' => DBField::create_field('Text', $searchURL),
+			'RSSLink' => DBField::create_field('Text', $rssUrl),
+			'AtomLink' => DBField::create_field('Text', $atomUrl)
+		);
+
+		$templates = array('FAQPage_results', 'Page');
+		if ($this->request->getVar('format') == 'rss') {
+			array_unshift($templates, 'Page_results_rss');
+		}
+		if ($this->request->getVar('format') == 'atom') {
+			array_unshift($templates, 'Page_results_atom');
+		}
+
+		return $this->owner->customise($renderData)->renderWith($templates);
+	}
+
+	/**
+	 * Expose variables to the template - both statics and data objects, and make the translatable where relevant.
+	 */
 	public function SearchFieldPlaceholder() {
 		return _t('FAQPage.SearchFieldPlaceholder', $this->SearchFieldPlaceholder);
 	}
