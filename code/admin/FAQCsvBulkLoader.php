a36b5a60fff4689dab15c2f958b263d5dbb1925b
Christopher Joe <cjoe@silverstripe.com>
2016-04-12 11:41:07 +1200

    Added config option to set whether to create categories if they're missing Also fix case sensitive check for children category search

--- code/admin/FAQCsvBulkLoader.php
+++ code/admin/FAQCsvBulkLoader.php
@@ -6,24 +6,22 @@ class FAQCsvBulkLoader extends CsvBulkLoader
 {
 
     public $columnMap = array(
-    'Question' => 'Question',
-    'Answer' => 'Answer',
-    'Keywords' => 'Keywords',
-    'Category' => '->getCategoryByName'
+        'Question' => 'Question',
+        'Answer' => 'Answer',
+        'Keywords' => 'Keywords',
+        'Category' => '->getCategoryByName'
     );
 
     public $duplicateChecks = array(
-    'Question' => 'Question'
+        'Question' => 'Question'
     );
 
     /**
-     * Avoids creating new categories if not found in the root taxonomy
+     * Avoids creating new categories if not found in the root taxonomy by default.
      * It will get the right CategoryID link, or leave the FAQ without categories.
      */
     public static function getCategoryByName(&$obj, $val, $record)
     {
-        $val = trim($val);
-
         $root = FAQ::getRootCategory();
         if (!$root || !$root->exists()) {
             return null;
@@ -31,9 +29,18 @@ class FAQCsvBulkLoader extends CsvBulkLoader
 
         $category = $root->getChildDeep(array('Name' => trim($val)));
 
+        if ((!$category || !$category->exists()) && trim($val) && Config::inst()->get('FAQ', 'create_missing_category')) {
+            $category = new TaxonomyTerm(array(
+                'Name' => trim($val),
+                'ParentID' => $root->ID
+            ));
+            $category->write();
+        }
+
         if ($category && $category->exists()) {
             $obj->CategoryID = $category->ID;
             $obj->write();
         }
+
     }
 }
