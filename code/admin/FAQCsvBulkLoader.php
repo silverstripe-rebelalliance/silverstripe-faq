6d4535aa76e37ef4128dd6f5fa23d753d8ee267f
Christopher Joe <cjoe@silverstripe.com>
2016-04-14 16:54:50 +1200

    Format answers to html if it doesn't already contain html

--- code/admin/FAQCsvBulkLoader.php
+++ code/admin/FAQCsvBulkLoader.php
@@ -7,7 +7,7 @@ class FAQCsvBulkLoader extends CsvBulkLoader
 
     public $columnMap = array(
         'Question' => 'Question',
-        'Answer' => 'Answer',
+        'Answer' => '->formatAnswer',
         'Keywords' => 'Keywords',
         'Category' => '->getCategoryByName'
     );
@@ -17,8 +17,37 @@ class FAQCsvBulkLoader extends CsvBulkLoader
     );
 
     /**
+     * Formats the answer into HTML, by replacing new lines characters with <br />
+     * Does not format if value already contains html
+     *
+     * @param $obj
+     * @param $val
+     * @param $record
+     * @return string
+     */
+    public function formatAnswer(&$obj, $val, $record)
+    {
+        // is this already html?
+        if (preg_match("/<[^<a-zA-Z]+>/", $val) === 1 ) {
+            $answer = $val;
+        }
+        else {
+            $answer = '<p>' . nl2br(Convert::raw2xml($val)) . '</p>';
+        }
+
+        $obj->Answer = $answer;
+        $obj->write();
+
+        return $answer;
+    }
+
+    /**
      * Avoids creating new categories if not found in the root taxonomy by default.
      * It will get the right CategoryID link, or leave the FAQ without categories.
+     * @param $obj
+     * @param $val
+     * @param $record
+     * @return string
      */
     public static function getCategoryByName(&$obj, $val, $record)
     {
@@ -41,6 +70,6 @@ class FAQCsvBulkLoader extends CsvBulkLoader
             $obj->CategoryID = $category->ID;
             $obj->write();
         }
-
+        return $category;
     }
 }
