e82996fa080a45d7c3a0272f75b4d505093bc63e
Christopher Joe <cjoe@silverstripe.com>
2016-04-21 16:16:26 +1200

    Toggle submit button disabled

--- javascript/faq.js
+++ javascript/faq.js
@@ -7,23 +7,34 @@
         var $form = $('.faq__rating'),
             $usefuls = $form.find('input[name="Useful"]'),
             $comment = $form.find('#Comment'),
-            $actions = $form.find('.Actions')
+            $actions = $form.find('.action'),
             existingComment = null;
 
-        $form.on('change', function () {
+        $form.on('change', showHideComment);
+
+        showHideComment();
+
+        function showHideComment() {
             var $useful = $usefuls.filter(':checked');
 
             if ($useful.val() === 'Y') {
                 existingComment = $comment.find(':input').val();
                 $comment.find(':input').val('');
+
                 $comment.hide();
+                $actions.prop('disabled', false);
             }
-            else {
+            else if ($useful.val() === 'N') {
                 if (existingComment) {
                     $comment.find(':input').val(existingComment);
                 }
                 $comment.show();
+                $actions.prop('disabled', false);
             }
-        }).change();
+            else {
+                $comment.hide();
+                $actions.prop('disabled', true);
+            }
+        }
     });
 }(jQuery));
